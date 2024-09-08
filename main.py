72333b806e9cec09daebe06b835afd1bc4cf8644
João Victor <elietejvc@hotmail.com>
2024-09-08 19:33:25 -0300

    Adicionando Funções

--- main.py
+++ main.py
@@ -8,32 +8,35 @@ counter_assassin = ("seraph", "zhonyas")
 counter_tank = ("liandry", "void")
 
 
-while True:
-    champions = input("Digite seu campeão: ").lower()
+def search_champions():
+    while True:
+        champions = input("Digite seu campeão: ").lower()
 
-    # Verificar se o campeão está nas listas
-    is_tank = champions in list_tank
-    is_assassin = champions in list_assassin
+        # Verificar se o campeão está nas listas
+        is_tank = champions in list_tank
+        is_assassin = champions in list_assassin
 
-    # Determinar a classificação
-    if is_tank and is_assassin:
-        classification = "tank-assassin"
-    elif is_assassin:
-        classification = "assassin"
-    elif is_tank:
-        classification = "tank"
-    else:
-        classification = "unknown"
+        # Determinar a classificação
+        if is_tank and is_assassin:
+            classification = "tank-assassin"
+        elif is_assassin:
+            classification = "assassin"
+        elif is_tank:
+            classification = "tank"
+        else:
+            classification = "unknown"
 
-    # Imprimir a build com base na classificação
-    if classification == "tank":
-        print("Build para tank:", counter_tank)
-        break
-    elif classification == "assassin":
-        print("Build contra assassin:", counter_assassin)
-        break
-    elif classification == "tank-assassin":
-        print("Build para tank:", counter_tank + counter_assassin)
-        break
-    else:
-        print("Esse campeão não existe na lista, tente novamente.")
+        # Imprimir a build com base na classificação
+        if classification == "tank":
+            print("Build para tank:", counter_tank)
+            break
+        elif classification == "assassin":
+            print("Build contra assassin:", counter_assassin)
+            break
+        elif classification == "tank-assassin":
+            print("Build para tank:", counter_tank + counter_assassin)
+            break
+        else:
+            print("Esse campeão não existe na lista, tente novamente.");
+
+search_champions()
