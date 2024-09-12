e09e4c0fff98c4da652aae0f1b78273ff96487c6
João Victor <elietejvc@hotmail.com>
2024-09-12 03:27:30 -0300

    Finalizando impressão das builds

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from routes.home import home_route
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.register_blueprint(home_route)
 
 if __name__ == "__main__":
--- static/main.js
+++ static/main.js
@@ -1,4 +1,4 @@
-function getBuild() {
+function getBuild() { 
     const champion = document.getElementById('champion').value;
 
     fetch("/get_build", {
@@ -11,17 +11,38 @@ function getBuild() {
     .then(response => response.json())
     .then(data => {
         const resultDiv = document.getElementById('result');
-        resultDiv.innerHTML = 'Build Recomendada: ';
-    data.build.forEach(item => {
-        const itemName = item[0];
-        const itemImage = item[1];        
+        resultDiv.innerHTML = '<h3>Build Recomendada: </h3>';
 
-    const itemDiv = document.createElement('div');
-          itemDiv.innerHTML = `<img src="${itemImage}" alt="${itemName}" style="width:50px; height:50px;"> ${itemName}`;
-          resultDiv.appendChild(itemDiv);
+        const container = document.createElement('div');
+        container.classList.add('result-container');
+
+        data.build.forEach(item => {
+            const itemName = item[0];
+            const itemImage = item[1];        
+
+            const itemContainer = document.createElement('div');
+            itemContainer.classList.add('item-container');
+
+            const imgElement = document.createElement('img');
+            imgElement.src = itemImage;
+            imgElement.alt = itemName;
+            imgElement.classList.add('item-img');
+            
+            const itemText = document.createElement('span');
+            itemText.textContent = itemName;
+            itemText.classList.add('item-name');
+
+            // Adicionar a imagem e o nome ao contêiner
+            itemContainer.appendChild(imgElement);
+            itemContainer.appendChild(itemText);
+
+            // Adicionar o contêiner ao container
+            container.appendChild(itemContainer);
         });
-    })
 
+        // Adicionar o container ao resultDiv
+        resultDiv.appendChild(container);
+    })
     .catch(error => console.error('Erro:', error));
 }
 
