49d77b15216447f744cf13084d4e121f51d4dcf5
João Victor <elietejvc@hotmail.com>
2024-09-17 19:28:09 -0300

    Atualizando lista de builds

--- routes/home.py
+++ routes/home.py
@@ -10,14 +10,16 @@ list_assassin = ["zed", "ahri", "akali", "annie", "diana", "fizz", "qyiana", "ka
 list_ranged = ["jayce", "tristana", "corki", "zeri", "lucian", "smolder", "akshan", "ezreal"]
 
 builds = {
-    "default": [("luden", "static/images/items/luden.png"), ("rabadon", "/static/images/items/rabadon.png")],
-    "counter_assassin": [("seraph", "static/images/items/Cajado_do_Arcanjo_item.webp"), ("zhonyas", "/static/images/items/zhonias.png")],
-    "counter_tank": [("liandry", "static/images/items/liandry.png"), ("void", "/static/images/items/cajadoVazio.png")]
+    "default": [("Companheiro de Luden", "static/images/items/luden.png"), ("Botas ionianas da lucidez", ""), ("Foco do Horizonte", ""), ("Capuz da Morte de Rabadon", "/static/images/items/rabadon.png"), ("Criptoflora", ""), ("Véu de Banshee", "")],
+    "counter_assassin": [("Abraço de Seraph", "static/images/items/Cajado_do_Arcanjo_item.webp"), ("Botas ionianas de Lucidez", ""), ("Zhonias", "/static/images/items/zhonias.png "), ("Rabadon", ""), ("Criptoflora", ""), ("Chama Sombria", "")],
+    "counter_tank": [("Abraço de Seraph", "static/images/items/Cajado_do_Arcanjo_item.webp"), ("Botas ionianas de Lucidez", ""), ("Angústia de Liandry", "static/images/items/liandry.png"), ("Cajado do Vazio", "/static/images/items/cajadoVazio.png"), ("Capuz da Morte de Rabadon", "/static/images/items/rabadon.png"), ("Ladrão de Almas Mejai", "")],
+    "counter_bruiser": [("Abraço de Seraph", "static/images/items/Cajado_do_Arcanjo_item.webp"), ("Botas Galvanizadas de Aço", ""), ("Angústia de Liandry", "static/images/items/liandry.png"), ("Capuz da Morte de Rabadon", "/static/images/items/rabadon.png"),  ("Criptoflora", ""), ("Zhonias", "/static/images/items/zhonias.png ")]
 }
 
 runes = {
     "default": [("static/images/runes/runaimg-removebg-preview.png")],
-    "counter_assassin": [("static/images/runes/counter_assassin.png")]
+    "counter_assassin": [("static/images/runes/counter_assassin.png")],
+    "counter_tank": [("static/images/runes/counter_tank.png")]
 }
 
 @home_route.route('/')
@@ -32,15 +34,19 @@ def get_build():
     # Verificar a classificação do campeão
     is_tank = champion in list_tank
     is_assassin = champion in list_assassin
+    is_bruiser = champion in list_bruiser
 
     if is_tank and is_assassin:
         build = builds["counter_tank"] + builds["counter_assassin"]
         rune = runes["default"]  # Escolha padrão se ambos se aplicam
     elif is_tank:
         build = builds["counter_tank"]
-        rune = runes["default"]
+        rune = runes["counter_tank"]
     elif is_assassin:
         build = builds["counter_assassin"]
+        rune = runes["counter_assassin"]
+    elif is_bruiser:
+        build = builds["counter_bruiser"]
         rune = runes["counter_assassin"]
     else:
         build = builds["default"]
